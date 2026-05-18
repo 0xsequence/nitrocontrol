599126b2f041a1669167639d3177941c337ad766
Patryk Kalinowski <pkalinowski@polygon.technology>
2026-05-18 12:43:28 +0200

    M-04: remove redundant io.ReadAll

--- attestation/userdata.go
+++ attestation/userdata.go
@@ -1,11 +1,9 @@
 package attestation
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
-	"io"
 	"net/http"
 )
 
@@ -24,20 +22,10 @@ func (u *userData) String() string {
 	return fmt.Sprintf("%s/%d:%s", u.Prefix, u.Version, base64.StdEncoding.EncodeToString(u.Hash))
 }
 
-func generateUserData(r *http.Request, resBody []byte) ([]byte, error) {
+func generateUserData(r *http.Request, reqBody []byte, resBody []byte) ([]byte, error) {
 	hasher := sha256.New()
 	hasher.Write([]byte(r.Method + " " + r.URL.Path + "\n"))
-
-	var reqBody []byte
-	var err error
-	if r.Body != nil {
-		reqBody, err = io.ReadAll(r.Body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read request body: %w", err)
-		}
-		r.Body = io.NopCloser(bytes.NewBuffer(reqBody))
-		hasher.Write(reqBody)
-	}
+	hasher.Write(reqBody)
 	hasher.Write([]byte("\n"))
 	hasher.Write(resBody)
 	hash := hasher.Sum(nil)
